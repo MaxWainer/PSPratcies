3590e9c709bc77e32f407d92262797b4e6fdd709
Ilya Koreisha <koreisha2000@mail.ru>
2020-12-23 01:12:42 +0300

    Фикс трабла в 74 номере, теперь пропускает с нормальными условиями. Ебанутых эксепшинов больше не будет. Или 2 или 3 аргумент, остальное просто идёт в помойку...

--- main/src/pshell/n74.ps1
+++ main/src/pshell/n74.ps1
@@ -2,10 +2,10 @@
 # то мы делаем всё что ниже до else
 # Подробнее: https://docs.microsoft.com/ru-ru/powershell/scripting/learn/deep-dives/everything-about-if?view=powershell-7.1
 # $args.Count - число аргументов
-if ( $args.Count -ne 2 ) { # ТУТ ПРОСТО ЗАЩИТА ОТ ДУРАКОВ!1!1!
+if ($args.Count -lt 2 -or $args.Count -gt 3 ) { # ТУТ ПРОСТО ЗАЩИТА ОТ ДУРАКОВ!1!1!
     Write-Host "Количество аргументов введено не верно!" # Вывод сообщений
     Write-Host "Вы ввели"$args.Count", а нужно 2 или 3!"
-    throw "Ошибка! Проверьте синтаксис команды!"; # Выход из скрипта, для удобства я просто выдаю "исключение"
+    throw "Ошибка! Проверьте синтаксис команды!" # Выход из скрипта, для удобства я просто выдаю "исключение"
 } else {
     # Присваиваем filePath к первому аргументу
     $path = $args[0]
@@ -18,21 +18,19 @@ if ( $args.Count -ne 2 ) { # ТУТ ПРОСТО ЗАЩИТА ОТ ДУРАКОВ!1!1!
     else {
         $date = $args[1]
     }
-    #Get-ChildItem -Path $path -Recurse -Include *.exe | where -FilterScript {
-    #   ($_.LastWriteTime -gt $date) -and ($_.Length -ge 1kb) -and ($_.Length -le 10mb)
-    #}
     Write-Host " "
     Write-Host "Директория:"$path
     Write-Host " "
     Write-Host "--------------------------------------------------------------------"
     Write-Host " "
-    Write-Host ""
-    $list = Get-ChildItem $path -Recurse | Where-Object -FilterScript {
-        ($_.LastAccessTime -gt $date)
-    }
+    Write-Host " Название Файла    Время последнего редактирования     Время достпа"
     Write-Host " "
+    $list = Get-ChildItem $path -Recurse | Where-Object -FilterScript {
+        ($_.CreationTime -like $date)
+    }
+    Write-Host $date
     ForEach ($item in $list) {
-        Write-Host ""$item.FullName "     " $item.LastWriteTime "      " $item.LastAccessTime
+        Write-Host ""$item.FullName "     " $item.LastWriteTimeUtc "      " $item.LastWriteTimeUtc""
     }
     Write-Host " "
     Write-Host "--------------------------------------------------------------------"
