81d85623b0fe84eee226ee0e0ed3f8164cf83cb3
Ilya Koreisha <koreisha2000@mail.ru>
2020-12-23 00:07:11 +0300

    Initial commit

---
+++ main/src/n74.ps1
@@ -0,0 +1,37 @@
+# -ne - означает если кол-во аргументов не равно 2
+# то мы делаем всё что ниже до else
+# Подробнее: https://docs.microsoft.com/ru-ru/powershell/scripting/learn/deep-dives/everything-about-if?view=powershell-7.1
+# $args.Count - число аргументов
+if ( $args.Count -ne 3 ) { # ТУТ ПРОСТО ЗАЩИТА ОТ ДУРАКОВ!1!1!
+    Write-Host "Количество аргументов введено не верно!" # Вывод сообщений
+    Write-Host "Вы ввели"$args.Count", а нужно 2 или 3!"
+    throw "Ошибка! Проверьте синтаксис команды!" # Выход из скрипта, для удобства я просто выдаю "исключение"
+} else {
+    # Присваиваем filePath к первому аргументу
+    $path = $args[0]
+    # Присваиваем date к второму аргументу
+    if ( $args.Count -eq 2 ) {
+        $date = $args[1]
+    } else {
+        $date = $args[1] + " " + $args[2]
+    }
+    #Get-ChildItem -Path $path -Recurse -Include *.exe | where -FilterScript {
+    #   ($_.LastWriteTime -gt $date) -and ($_.Length -ge 1kb) -and ($_.Length -le 10mb)
+    #}
+    Write-Host " "
+    Write-Host "Directory:"$path
+    Write-Host " "
+    Write-Host "-------------------------------------------------------------------"
+    Write-Host " "
+    Write-Host " File name             Last Write Time             Last Access Time"
+    $list = Get-ChildItem $path -Recurse | Where-Object -FilterScript {
+        ($_.LastAccessTime -gt $date)
+    }
+    Write-Host " "
+    ForEach ($item in $list) {
+        Write-Host ""$item.FullName "     " $item.LastWriteTime "      " $item.LastAccessTime
+    }
+    Write-Host " "
+    Write-Host "-------------------------------------------------------------------"
+    Write-Host "Current time:"System.DateTime.ToString
+}
